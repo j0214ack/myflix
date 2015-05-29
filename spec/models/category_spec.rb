11bfc661469001a5018712ed18bc48a7eef0d267
Jack Chen <lalala.jack@gmail.com>
2015-05-29 15:28:56 -0500

    Use shoulda-matchers instead

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,14 +8,5 @@ describe Category do
     expect(category.valid?).to eq(true)
   end
 
-  it "has many videos" do
-    category = Category.create!(name: "A Category")
-    video1 = Video.create!(title: "Lost", description: "A mysterious TV serie.",
-                           category: category)
-    video2 = Video.create!(title: "Lost2", description: "A mysterious TV serie2.",
-                           category: category)
-
-    expect(category.videos.size).to eq(2)
-    expect(category.videos).to include(video1, video2)
-  end
+  it { should have_many :videos }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -8,12 +8,10 @@ describe Video do
     expect(video.valid?).to eq(true)
   end
 
-  it "belongs to a category" do
-    category = Category.create!(name: "my_category")
-    video = Video.create!(title: "Lost", description: "A mysterious TV serie.",
-                          category: category)
+  it { should belong_to :category }
 
-    expect(video.valid?).to eq(true)
-    expect(video.category).to eq(category)
-  end
+  it { should validate_presence_of :title }
+
+  it { should validate_presence_of :description }
+
 end
