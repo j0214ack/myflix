578172574919f5765f555f205bf770eecd1aedcc
Jack Chen <lalala.jack@gmail.com>
2015-05-29 15:31:41 -0500

    Remove testing rails save mechanism

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "saves itself" do
-    category = Category.new(name: "Some Category")
-    category.save
-
-    expect(category.valid?).to eq(true)
-  end
-
   it { should have_many :videos }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,16 +2,7 @@ require 'spec_helper'
 
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Lost", description: "A mysterious TV serie.")
-    video.save
-    expect(video.valid?).to eq(true)
-  end
-
   it { should belong_to :category }
-
   it { should validate_presence_of :title }
-
   it { should validate_presence_of :description }
-
 end
