a3e7e4e4394b608e56848848fa46d827e4f75dc9
Jack Chen <lalala.jack@gmail.com>
2015-05-30 13:34:42 -0500

    Refactored a bit

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,7 +7,6 @@ class Video < ActiveRecord::Base
   scope :without_category, -> { where(category_id: nil) }
 
   def self.search_by_title(search_value)
-    wild_card_search = "%#{search_value.downcase}%"
-    Video.where("lower(title) LIKE ?", wild_card_search).order("created_at DESC")
+    where("lower(title) LIKE ?", "%#{search_value.downcase}%").order("created_at DESC")
   end
 end
