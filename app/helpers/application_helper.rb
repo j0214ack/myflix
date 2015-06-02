440fc0eab302e39981a60a23d3bcf79c1c5d7639
Jack Chen <lalala.jack@gmail.com>
2015-06-02 10:50:02 -0500

    Using my own bootstrap form builder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def bootstrap_form_for(record, options = {}, &block)
+    options.merge!(builder: MyBootstrapFormBuilder)
+    form_for(record, options, &block)
+  end
 end
---
+++ app/helpers/my_bootstrap_form_builder.rb
@@ -0,0 +1,74 @@
+class MyBootstrapFormBuilder < ActionView::Helpers::FormBuilder
+
+  def self.bootstrap_field(*method_names)
+    method_names.each do |method_name|
+      method_name = method_name.to_s
+      self.class_eval %Q{
+        def #{method_name}(method, options = \{\})
+          col_sm_num = options.delete(:col_sm)
+          the_#{method_name} = super(method, bootstrap_options(method, options))
+          @template.content_tag :div, class: ["col-sm-\#\{col_sm_num\}"] do
+            the_#{method_name} + field_errors(method)
+          end
+        end
+      }
+    end
+  end
+
+  bootstrap_field :text_field, :email_field, :password_field
+
+  def group(attr_sym, &block)
+    classes = %w(form-group)
+    classes << "has-error" if object.errors[attr_sym].any?
+    @template.content_tag :div, class: classes do
+      block.call
+    end
+  end
+
+  def label(method, text = nil, options = {}, &block)
+    col_sm_num = options.delete(:col_sm)
+    options.merge!(class: ["col-sm-#{col_sm_num}", "control-label"])
+    super(method, text, objectify_options(options), &block)
+  end
+
+  # def email_field(method, options = {})
+  #   col_sm_num = options.delete(:col_sm)
+  #   the_email_field = super(method, bootstrap_options(method, options))
+  #   @template.content_tag :div, class: ["col-sm-#{col_sm_num}"] do
+  #     the_email_field + field_errors(method)
+  #   end
+  # end
+  #
+  # def password_field(method, options = {})
+  #   col_sm_num = options.delete(:col_sm)
+  #   the_password_field = super(method, bootstrap_options(method, options))
+  #   @template.content_tag :div, class: ["col-sm-#{col_sm_num}"] do
+  #     the_password_field + field_errors(method)
+  #   end
+  # end
+
+  # def text_field(method, options = {})
+  #   col_sm_num = options.delete(:col_sm)
+  #   the_text_field = super(method, bootstrap_options(method, options))
+  #   @template.content_tag :div, class: ["col-sm-#{col_sm_num}"] do
+  #     the_text_field + field_errors(method)
+  #   end
+  # end
+
+  private
+
+  def bootstrap_options(method, options = {})
+    options.merge!(class: "form-control")
+    objectify_options(options)
+  end
+
+  def field_errors(method)
+    if object.errors.any?
+      error_msgs = object.errors[method].map do |err_msg|
+        @template.content_tag(:span, class: "help-block") do
+           err_msg
+        end
+      end.join.html_safe
+    end
+  end
+end
