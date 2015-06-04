cb3058d42f502a8863c328d8a71a3086a322a455
Jack Chen <lalala.jack@gmail.com>
2015-06-04 09:33:16 -0500

    Modified videos_controller test by solution

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,106 +1,50 @@
 require 'spec_helper'
 
 def login_user
-  # user = User.find_by(email: "a@example.com")
-  user ||= User.create(email: "a@example.com", password: "abc", full_name: "CY C")
-  request.session[:user_id] = user.id
+  session[:user_id] = Fabricate(:user).id
 end
 
 describe VideosController do
-
   describe "GET show" do
-
-    it "sets @video variable" do
+    it "sets @video variable when user is authenticated" do
       login_user
-      south_park = Video.create(title: "South Park", description: "Stain, Eric")
+      south_park = Fabricate(:video, title: "South Park")
       get :show, id: south_park.id
       expect(assigns(:video)).to eq(south_park)
     end
 
-    it "renders videos/show template" do
-      login_user
-      south_park = Video.create(title: "South Park", description: "Stain, Eric")
+    it "redirect to root path when user is not authenticated" do
+      south_park = Fabricate(:video, title: "South Park")
       get :show, id: south_park.id
-
-      expect(response).to render_template :show
+      expect(response).to redirect_to root_path
     end
-
   end
 
   describe "GET search" do
+    context "when user is authenticated" do
+      before(:each) { login_user }
+      it "sets @search_value variable from parameter" do
+        login_user
+        get :search, q: "South Park"
 
-    it "sets @search_value variable from parameter" do
-      login_user
-      get :search, q: "South Park"
-
-      expect(assigns(:search_value)).to eq("South Park")
-    end
-
-    it "renders search template" do
-      login_user
-      get :search, q: "South Park"
-
-      expect(response).to render_template :search
-    end
-
-    describe "@search_results" do
-      context "query no match" do
-        it "is set to []" do
-          login_user
-          monk = Video.create(title: "Monk", description: "Monk")
-          get :search, q: "South Park"
-          expect(assigns(:search_results)).to eq([])
-        end
+        expect(assigns(:search_value)).to eq("South Park")
       end
 
-      context "query matched only one video" do
-        it "is an array containing one element" do
-          login_user
-          monk = Video.create(title: "Monk", description: "Monk")
-          get :search, q: "monk"
-          expect(assigns(:search_results)).to eq([monk])
-        end
+      it "sets @search_results" do
+        login_user
+        south_park = Fabricate(:video, title: "South Park")
+        get :search, q: "South Park"
+
+        expect(assigns(:search_results)).to eq([south_park])
       end
+    end # context "user authenticated"
 
-      context "query matched three videos" do
-        it "is an array containing all three videos" do
-          login_user
-          monk = Video.create(title: "Monk", description: "Monk")
-          monk2 = Video.create(title: "Monk 2", description: "Monk")
-          monk3 = Video.create(title: "Monk 3", description: "Monk")
-          get :search, q: "monk"
-          expect(assigns(:search_results)).to include(monk, monk2, monk3)
-          expect(assigns(:search_results).size).to eq(3)
-        end
+    context "when user is not authenticated" do
+      it "redirects to root path" do
+        get :search, q: "South Park"
 
-        it "returns the three matched videos in descending order of created_at" do
-          login_user
-          monk = Video.create(title: "Monk", description: "Monk")
-          monk2 = Video.create(title: "Monk 2", description: "Monk")
-          monk3 = Video.create(title: "Monk 3", description: "Monk")
-          get :search, q: "monk"
-          expect(assigns(:search_results)).to eq([monk, monk2, monk3].reverse)
-        end
-      end
-
-      context "query is empty string" do
-        it "is an empty array" do
-          login_user
-          monk = Video.create(title: "Monk", description: "Monk")
-          get :search, q: ""
-          expect(assigns(:search_results)).to eq([])
-        end
-      end
-
-      context "query is nil" do
-        it "is an empty array" do
-          login_user
-          monk = Video.create(title: "Monk", description: "Monk")
-          get :search, q: nil
-          expect(assigns(:search_results)).to eq([])
-        end
+        expect(response).to redirect_to root_path
       end
     end
-
   end
 end
--- spec/fabricators/video_fabricator.rb
+++ spec/fabricators/video_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:video) do
-  title { Fake::Lorem.word(2).join(" ") }
-  description { Fake::Lorem.paragraph(2) }
+  title { Faker::Lorem.word(2).join(" ") }
+  description { Faker::Lorem.paragraph(2) }
 end
