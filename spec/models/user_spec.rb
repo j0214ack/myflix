501de28b44029e4f094e24a9a148c9cba4f13837
Jack Chen <lalala.jack@gmail.com>
2015-06-04 18:40:50 -0500

    Used another shoulda matcher for password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_uniqueness_of :email }
   it { is_expected.to validate_presence_of :password }
-  it { is_expected.to validate_presence_of :password_confirmation }
+  it { is_expected.to validate_confirmation_of :password }
   it { is_expected.to validate_presence_of :full_name }
   it { is_expected.to have_secure_password }
 end
