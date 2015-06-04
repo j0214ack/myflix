de1db830ca300bbcb586d759c2f4f7078362b0dc
Jack Chen <lalala.jack@gmail.com>
2015-06-04 17:37:25 -0500

    Fixed something

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
+    let(:south_park) { Fabricate(:video, title: "South Park") }
+
     it "sets @video variable when user is authenticated" do
       login_user
-      south_park = Fabricate(:video, title: "South Park")
       get :show, id: south_park.id
       expect(assigns(:video)).to eq(south_park)
     end
 
     it "redirect to root path when user is not authenticated" do
-      south_park = Fabricate(:video, title: "South Park")
       get :show, id: south_park.id
       expect(response).to redirect_to root_path
     end
@@ -19,21 +19,20 @@ describe VideosController do
   describe "GET search" do
     context "when user is authenticated" do
       before(:each) { login_user }
+
       it "sets @search_value variable from parameter" do
-        login_user
         get :search, q: "South Park"
 
         expect(assigns(:search_value)).to eq("South Park")
       end
 
       it "sets @search_results" do
-        login_user
         south_park = Fabricate(:video, title: "South Park")
         get :search, q: "South Park"
 
         expect(assigns(:search_results)).to eq([south_park])
       end
-    end # context "user authenticated"
+    end 
 
     context "when user is not authenticated" do
       it "redirects to root path" do
