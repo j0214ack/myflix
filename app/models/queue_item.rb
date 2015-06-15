dc68e8f95146dc2b865f68654c288801a7c42604
Jack Chen <lalala.jack@gmail.com>
2015-06-15 10:56:09 -0500

    Automatically adds position value to queue item

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,10 +1,23 @@
 class QueueItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :video
+  validates_uniqueness_of :position, scope: :user_id
+  before_validation :set_position, unless: :has_position?
+
   delegate :category, to: :video
 
   def rating
     review = video.reviews.find_by(user_id: user.id)
     review.rating if review
+  end
+
+  private
+
+  def set_position
+    self.position = QueueItem.where(user_id: self.user_id).count + 1
+  end
+
+  def has_position?
+    !!self.position
   end
 end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,6 +3,40 @@ require 'spec_helper'
 describe QueueItem do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :video }
+  it { is_expected.to validate_uniqueness_of(:position).scoped_to(:user_id)}
+  it { is_expected.to callback(:set_position).before(:validation).unless(:has_position?) }
+
+  describe '#set_position' do
+    it 'sets position' do
+      queue_item = QueueItem.new
+      queue_item.send(:set_position)
+      expect(queue_item.position).to be
+    end
+
+    it 'sets unique positions for queue items which belong to the same user' do
+      user = Fabricate(:user)
+      queue_items = Fabricate.times(5, :queue_item, user: user)
+      queue_item = QueueItem.new(user: user)
+      queue_item.send(:set_position)
+
+      expect(queue_items.map(&:position)).not_to include(queue_item.position)
+    end
+  end
+
+  describe '#has_position?' do
+    it 'returns true when queue item has a position value' do
+      queue_item = QueueItem.new
+      queue_item.position = 1
+
+      expect(queue_item.send(:has_position?)).to be true
+    end
+
+    it 'returns false when queue item has no position value' do
+      queue_item = QueueItem.new
+
+      expect(queue_item.send(:has_position?)).to be false
+    end
+  end
 
   describe '#rating' do
     it 'shows the rating of its video' do
